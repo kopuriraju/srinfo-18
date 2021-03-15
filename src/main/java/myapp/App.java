6a94b005209bee9c16e8e04553ab56873750d9e0
kopuriraju <rajkopuri@gmail.com>
2021-03-15 09:29:39 +0530

    helloworld using maven

---
+++ src/main/java/myapp/App.java
@@ -0,0 +1,13 @@
+package myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Devops engineers! version 1.1" );
+    }
+}
